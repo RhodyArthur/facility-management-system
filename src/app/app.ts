e508ccda5e03fd33c7b7ff17cf25a6637a930749
RhodyArthur <rhodaarthur48@gmail.com>
2026-07-15 21:39:31 +0000

    feat: add ToastModule integration and update app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, ToastModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
